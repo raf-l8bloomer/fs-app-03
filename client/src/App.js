1b8ecfc2a937651b3c223520faf7a1ccd61117d4
rafael.lumang <lumang.rafael@gmail.com>
2023-09-13 13:42:56 -0700

    Connected React to REST API

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios"
 
 function App() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/courses")
+      .then(response => {
+        // handle success
+        setCourses(response.data);
+      })
+      .catch(error => {
+        // handle error
+        console.log("Error fetching and parsing data", error);
+      })
+    },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ul>
+      {courses.map((course) => {
+        return(
+          <li>{course.title}</li>
+        )
+      })}
+    </ul>
+    </>
   );
 }
 

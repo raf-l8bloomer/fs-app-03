7d533909879b18f039318e7b9acab1be1a4872e5
rafael.lumang <lumang.rafael@gmail.com>
2023-09-14 22:58:03 -0700

    added Create Course logic + ignored client/nodemodules

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Route, Routes,  } from "react-router-dom"
-
+import { Route, Routes, } from "react-router-dom"
 
 import Header from "./components/Header";
 import Courses from "./components/Courses";
@@ -10,24 +9,27 @@ import CourseDetail from "./components/CourseDetail";
 import UserSignIn from "./components/UserSignIn";
 import UserSignUp from "./components/UserSignUp";
 import UserSignOut from "./components/UserSignOut";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
 
   return (
-      <div id="root">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Courses />} />
+    <div id="root">
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Courses />} />
+          <Route path="/courses/:id" element={<CourseDetail />} />
+          <Route path="/signin" element={<UserSignIn />} />
+          <Route path="/signup" element={<UserSignUp />} />
+          <Route path="/signout" element={<UserSignOut />} />
+          <Route element={<PrivateRoute />}>
             <Route path="/courses/create" element={<CreateCourse />} />
             <Route path="/courses/:id/update" element={<UpdateCourse />} />
-            <Route path="/courses/:id" element={<CourseDetail />} />
-            <Route path="/signin" element={<UserSignIn />} />
-            <Route path="/signup" element={<UserSignUp />} />
-            <Route path="/signout" element={<UserSignOut />} />
-          </Routes>
-        </main>
-      </div>
+          </Route>
+        </Routes>
+      </main>
+    </div>
 
   );
 }
--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -1,42 +1,116 @@
 /**
  * renders a form allows user to create a new course
  * renders "Create Course" button that sends a POST request to /api/courses
- * renders "Cance" button that returns to list of courses
+ * renders "Cancel" button that returns to list of courses
  */
 
-const CreateCourse = () => {
+import { useNavigate } from "react-router-dom"
+import { useRef, useState, useContext} from "react";
+import UserContext from "../context/UserContext";
 
+const CreateCourse = () => {
+    const { authUser } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    const courseTitle = useRef(null);
+    const courseDescription = useRef(null);
+    const estimatedTime = useRef(null);
+    const materialsNeeded = useRef(null);
+    const [errors, setErrors] = useState([]);
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const course = {
+            courseTitle: courseTitle.current.value,
+            courseDescription: courseDescription.current.value,
+            estimatedTime: estimatedTime.current.value,
+            materialsNeeded: materialsNeeded.current.value,
+        }
+
+        const fetchOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json; charset=utf-8"
+            },
+            body: JSON.stringify(course)
+        }
+
+        try {
+            const response = await fetch("http://localhost:5000/api/courses", fetchOptions);
+            if (response.status === 201) {
+                console.log("course was made!:" + course)
+                navigate("/");
+            } else if (response.status === 400) {
+                const data = await response.json();
+                setErrors(data.errors);
+            } else {
+                throw new Error();
+            }
+        } catch (error) {
+            console.log(error);
+            navigate("/");
+        }
+}
+
+
+    const handleCancel = (e) => {
+        e.preventDefault();
+        navigate("/");
+    }
 
     return (
         <div className="wrap">
             <h2>Create Course</h2>
-            <div className="validation--errors">
+
+            { errors.length ? (
+                <div className="validation--errors">
                 <h3>Validation Errors</h3>
                 <ul>
                     <li>Please provide a value for "Title"</li>
                     <li>Please provide a value for "Description"</li>
                 </ul>
-            </div>
-            <form>
+            </div> 
+            ) : null }
+
+            <form onSubmit={authUser ? (handleSubmit) : null}>
                 <div className="main--flex">
                     <div>
-                        <label for="courseTitle">Course Title</label>
-                        <input id="courseTitle" name="courseTitle" type="text" value="" />
+                        <label htmlFor="courseTitle">Course Title</label>
+                        <input
+                            id="courseTitle"
+                            name="courseTitle"
+                            type="text"
+                            ref={courseTitle}
+                        />
 
                         <p>By Joe Smith</p>
 
-                        <label for="courseDescription">Course Description</label>
-                        <textarea id="courseDescription" name="courseDescription"></textarea>
+                        <label htmlFor="courseDescription">Course Description</label>
+                        <textarea
+                            id="courseDescription"
+                            name="courseDescription"
+                            ref={courseDescription}></textarea>
                     </div>
                     <div>
-                        <label for="estimatedTime">Estimated Time</label>
-                        <input id="estimatedTime" name="estimatedTime" type="text" value="" />
+                        <label htmlFor="estimatedTime">Estimated Time</label>
+                        <input
+                            id="estimatedTime"
+                            name="estimatedTime"
+                            type="text"
+                            ref={estimatedTime}
+                        />
 
-                        <label for="materialsNeeded">Materials Needed</label>
-                        <textarea id="materialsNeeded" name="materialsNeeded"></textarea>
+                        <label htmlFor="materialsNeeded">Materials Needed</label>
+                        <textarea
+                            id="materialsNeeded"
+                            name="materialsNeeded"
+                            ref={materialsNeeded}></textarea>
                     </div>
                 </div>
-                <button className="button" type="submit">Create Course</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
+                <button className="button" type="submit">Create Course</button>
+                <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
             </form>
         </div>
     )
